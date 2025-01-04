65f4f863bf18b79ae3bdca43d4900f318b11988e
Roan de Oliveira <roanrobersson@gmail.com>
2025-01-04 18:43:23 -0300

    feat: :heavy_plus_sign: install primereact

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+// import "primereact/resources/themes/lara-light-blue/theme.css";
+
 import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import SecondPage from "pages/Second";
+import { PrimeReactProvider } from "primereact/api";
 
 import DefaultLayout from "components/Layout";
 
@@ -13,16 +16,17 @@ export default function App() {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<DefaultLayout />}>
-						<Route index element={<HomePage />} />
-						<Route path="/second-page" element={<SecondPage />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-
-			<ReactQueryDevtools />
+			<PrimeReactProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<DefaultLayout />}>
+							<Route index element={<HomePage />} />
+							<Route path="/second-page" element={<SecondPage />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+				<ReactQueryDevtools />
+			</PrimeReactProvider>
 		</QueryClientProvider>
 	);
 }
